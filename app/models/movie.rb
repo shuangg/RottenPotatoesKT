af902ba3496745a5666fef716cb6294da31a77b2
Kevin Tham <kevin.tham@berkeley.edu>
2012-02-04 22:00:20 -0800

    Fixed SQL query error for Postgres deployment. Needed a :select option with :group

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ActiveRecord::Base
   def self.ratings
   	ratings = []
-    records = self.find(:all, :group => 'rating')
+    records = self.find(:all, :select => 'rating', :group => 'rating')
     records.each do |record|
     	ratings << record.rating
     end
